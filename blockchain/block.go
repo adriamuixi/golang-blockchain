c13b0d518bbd8133b11d6c09a59ee383e043d97b
adriamuixi <adriamuixi@gmail.com>
2021-04-15 20:39:42 +0200

    Modify proof and block

--- blockchain/block.go
+++ blockchain/block.go
@@ -24,8 +24,8 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, newBlock)
 }
 
 func Genesis() *Block {
--- blockchain/proof.go
+++ blockchain/proof.go
@@ -10,7 +10,7 @@ import (
 	"math/big"
 )
 
-const Difficulty = 17
+const Difficulty = 18
 
 type ProofOfWork struct {
 	Block  *Block
